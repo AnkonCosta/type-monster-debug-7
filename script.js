b68dc142ffc14954e71ba6937c11170f686e69e3
Ankon Costa <ankoncosta247@gmail.co>
2022-09-15 01:00:13 +0600

    added counter fix

--- script.js
+++ script.js
@@ -112,11 +112,12 @@ const start = () => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
-    if (count == 0) {
+    if (count < 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
+      countdownOverlay.innerHTML = '';
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
